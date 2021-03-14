490fc29010e342cbee2bb649869807412de2e0c9
AndreyBodulin <andrey_bodulin@mail.ru>
2021-03-14 16:54:00 +0400

    task: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, bracketsConfig) {
+  console.log(s);
+  let sum = 0;
+  const brackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+  // в качестве стека в JavaScript можно использовать обычный массив
+  //(если пользоваться только «разрешенными» методами push & pop)
+  const st = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '|') {
+      sum += 1;
+      continue;
+    };
+    if (isClosedBracket(s[i])) {
+      if (brackets[s[i]] !== st.pop()) return false;
+    // это открывающая скобка, т.к. других символов по условию в строке нет
+    } else {
+      st.push(s[i]);
+    }
+  }
+  if (sum % 2 == 0 && (s.indexOf('|') != -1)) return true;
+  console.log(sum);
+  return st.length === 0;
+  function isClosedBracket(ch) {
+    return [')', ']', '}'].indexOf(ch) > -1;
+  }
 }
